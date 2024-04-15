eb714ee0028de32e0406170730e61de4b19c4ac6
Akihisa Yamaguchi <71268501+albasiempre@users.noreply.github.com>
2024-04-15 22:51:22 +0900

    Update apiClient.ts

--- src/lib/apiClient.ts
+++ src/lib/apiClient.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "https://social-media-mdn-be.onrender.com",
-  // baseURL: process.env.NEXT_PUBLIC_API_BASEURL,
+  // baseURL: "https://social-media-mdn-be.onrender.com",
+  baseURL: process.env.NEXT_PUBLIC_API_BASEURL,
   headers: {
     "Content-Type": "application/json",
   },
